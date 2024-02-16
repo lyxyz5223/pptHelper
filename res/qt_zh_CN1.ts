33dd6ddccf9f8073b5242daa689562273cc78ef9
lyxyz5223 <104821273+lyxyz5223@users.noreply.github.com>
2024-02-16 15:49:58 +0800

    Add files via upload

---
+++ res/qt_zh_CN1.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+<name>QColorDialog</name>
+<message>
+<source>&amp;Basic colors</source>
+<translation>基本颜色(&amp;B)</translation>
+</message>
+
+<message>
+<source>&amp;Pick Screen Color</source>
+<translation>拾取屏幕颜色(&amp;P)</translation>
+</message>
+
+<message>
+<source>Cursor at %1, %2
+Press ESC to cancel</source>
+<translation>坐标: %1, %2
+按ESC键取消</translation>
+</message>
+
+<message>
+<source>&amp;Custom colors</source>
+<translation>自定义颜色(&amp;C)</translation>
+</message>
+
+<message>
+<source>&amp;Add to Custom Colors</source>
+<translation>添加自定义颜色(&amp;A)</translation>
+</message>
+
+<message>
+<source>Hu&amp;e:</source>
+<translation>色调(&amp;E):</translation>
+</message>
+
+<message>
+<source>&amp;Sat:</source>
+<translation>饱和度(&amp;S):</translation>
+</message>
+
+<message>
+<source>&amp;Val:</source>
+<translation>亮度(&amp;V):</translation>
+</message>
+
+<message>
+<source>&amp;Red:</source>
+<translation>红(&amp;R):</translation>
+</message>
+
+<message>
+<source>Bl&amp;ue:</source>
+<translation>蓝(&amp;U):</translation>
+</message>
+
+<message>
+<source>&amp;Green:</source>
+<translation>绿(&amp;G):</translation>
+</message>
+
+    <name>QPlatformTheme</name>
+    <message>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+</TS>
